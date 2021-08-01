729b06b1a7819f786a5f990596be10e1b47fcd7a
unknown <uv@TSOC-MIN706.coatza.intra.uv.mx>
2021-08-01 12:37:35 -0500

    FOOTER

---
+++ src/views/app/components/footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-footer class="text-center" padless>
+    <v-col class="error text-h6 font-weight-bold white--text" cols="12"
+      >COBAEV<span class="grey--text">.EDU.MX</span></v-col
+    >
+    <v-col class="text-body-2 white--text grey darken-4 py-0" cols="12">
+        Av. Américas 87, Colonia Aguacatal, C.P. 91130, Xalapa, Veracruz, México. Tel. (228) 842-3320
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "footer",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
