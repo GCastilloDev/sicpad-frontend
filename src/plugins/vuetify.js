5aff5e0a385f45b22800220b7b1298cbccc8037d
unknown <uv@TSOC-MIN706.coatza.intra.uv.mx>
2021-08-01 13:09:25 -0500

    LOGIN

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,7 +11,7 @@ export default new Vuetify({
       },
     themes: {
       light: {
-        primary: '#007BFF',
+        primary: '#5ea10e',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#b1382c',
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-        <h1>Login</h1>
-    </div>
+  <v-row class="login justify-center align-center">
+    <v-col cols="5">
+      <v-card>
+        <v-card-title class="font-weight-bold"> Inicio de sesión </v-card-title>
+        <v-card-text class="pb-0">
+          <v-form>
+            <v-text-field
+              label="Correo"
+              placeholder="Favor de ingresar su correo electrónico"
+              type="email"
+              append-icon="mdi-email"
+              dense
+              outlined
+            ></v-text-field
+            ><v-text-field
+              label="Contraseña"
+              placeholder="Favor de ingresar su contraseña"
+              type="password"
+              append-icon="mdi-shield-lock"
+              dense
+              outlined
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions class="d-block px-5">
+          <v-btn class="mb-3" color="primary" block depressed>Ingresar</v-btn>
+          <p class="ma-0 text-center text-overline">¿Olvidaste tu contraseña? Da click <a href="#">aquí</a></p>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
+.login {
+  min-height: 100%;
+}
 </style>
